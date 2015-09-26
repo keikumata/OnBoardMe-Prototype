c7307ce30dece37fed763b9e5135d6687b9ecf16
JaeHunRo <jhr24@duke.edu>
2015-09-26 22:13:43 +0100

    serves login

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-// var userController = require('./userController');
+var userController = require('./userController');
 var path = require('path');
 
 var bodyParser = require('body-parser');
@@ -16,7 +16,7 @@ app.use(express.static(path.resolve(__dirname + '/../client')));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-// app.get('/login', userController.loginPage);
+app.get('/login', userController.loginPage);
 // app.post('/login', userController.login);
 // app.post('/board', userController.createBoard);
 
