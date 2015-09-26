4f4d45f082597c7a8fe28a55b8a1ef2b29a8f71c
JaeHunRo <jhr24@duke.edu>
2015-09-26 22:07:57 +0100

    only serves index

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var userController = require('./userController');
+// var userController = require('./userController');
 var path = require('path');
 
 var bodyParser = require('body-parser');
@@ -16,8 +16,8 @@ app.use(express.static(path.resolve(__dirname + '/../client')));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/login', userController.loginPage);
-app.post('/login', userController.login);
-app.post('/board', userController.createBoard);
+// app.get('/login', userController.loginPage);
+// app.post('/login', userController.login);
+// app.post('/board', userController.createBoard);
 
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT);
