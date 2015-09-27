f32f8055a2c106ef11aa93a515cfa86d95302801
JaeHunRo <jhr24@duke.edu>
2015-09-27 10:40:36 +0100

    no partial load

--- client/js/landing.js
+++ client/js/landing.js
@@ -1,13 +1,8 @@
-var id = document.cookie.split('user_id=');
-if (id[1]) {
-	window.location = 'boards.html';
-} else {
-	var loginButton = document.getElementById('facebook-login');
-	loginButton.onclick = function() {
-		$.post('/log').then(function(response) {
-			window.location = 'boards.html';
-		})
-	};
-}
+var loginButton = document.getElementById('facebook-login');
+loginButton.onclick = function() {
+	$.post('/log').then(function(response) {
+		window.location = 'boards.html';
+	})
+};
 
 
