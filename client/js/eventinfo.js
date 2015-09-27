7470bb11964d47d2ac8783c057bb510e2c7cf6f1
keikumata <ky52@duke.edu>
2015-09-27 04:48:18 +0100

    change eventinfo

--- client/js/eventinfo.js
+++ client/js/eventinfo.js
@@ -1,29 +1,57 @@
-var place_title = document.getElementById('title');
-place_title.className = "title";
-place_title.innerHTML = "lol title";
+  var href = window.location.href;
+  var aid = href.split("aid=")[1];
+
+  $.get("/eventinfo?"+"aid="+aid, function(data) {
+  	var jsondata = JSON.parse(data);
+  	var eventobject = jsondata.events;
+  	var name = eventobject.name;
+  	var url = eventobject.img;
+  	var loc = eventobject.location;
+  	var price = eventobject.price;
+  	var coordinate= eventobject.coordinates;
+  	setUpDivs(name, aid, url, loc, price,coordinate);
+  })
 
 
-var event_title = document.getElementById("event-title");
-var fa_map_marker = document.createElement("i");
-fa_map_marker.className = "fa fa-map-marker";
-var city_name = document.createTextNode(" Camden");
-
-var space = document.createTextNode("  ");
-
-var fa_money = document.createElement("i");
-fa_money.className = "fa fa-money";
-var price = document.createTextNode(" £ 50"); 
 
 
-var place_stats = document.createElement("div");
+  function setUpDivs(name, aid, url, loc, price, coordinate) {
+  	var place_image = document.getElementById("place-image");
+  	place_image.style.backgroundImage = "url('" + url + ")'";
+  	place_image.className = "place-image";
 
-place_stats.className="place-stats";
+  	var place_title = document.getElementById('title');
+  	place_title.className = "title";
+  	place_title.innerHTML = name;
 
-place_stats.appendChild(fa_map_marker);
-place_stats.appendChild(city_name);
-place_stats.appendChild(space);
-place_stats.appendChild(fa_money);
-place_stats.appendChild(price);
 
-event_title.appendChild(place_stats);
+  	var event_title = document.getElementById("event-title");
+  	var fa_map_marker = document.createElement("i");
+  	fa_map_marker.className = "fa fa-map-marker";
+  	var city_name = document.createTextNode(" " + loc);
+
+  	var space = document.createTextNode("  ");
+
+  	var fa_money = document.createElement("i");
+  	fa_money.className = "fa fa-money";
+  	var price = document.createTextNode(" " + price); 
+
+
+  	var place_stats = document.createElement("div");
+
+  	place_stats.className="place-stats";
+
+  	place_stats.appendChild(fa_map_marker);
+  	place_stats.appendChild(city_name);
+  	place_stats.appendChild(space);
+  	place_stats.appendChild(fa_money);
+  	place_stats.appendChild(price);
+
+  	event_title.appendChild(place_stats);
+
+  }
+
+
+
+
 
