032abd27ffb2c7dc79e614556629d136e4457fbe
keikumata <ky52@duke.edu>
2015-09-27 01:29:43 +0100

    added dynamic change to pages

---
+++ client/board.js
@@ -0,0 +1,14 @@
+var dynamic = document.getElementById('dynamicboard');
+var names = ["London", "Paris", "Tokyo"];
+for (var i=0; i<names.length; i++) {
+	var a = document.createElement("a");
+	a.href = "single-board.html";
+	var div = document.createElement("div");
+	div.className="board";
+	var div_title = document.createElement("div");
+	div_title.className="board-title";
+	div_title.innerHTML = names[i];
+	div.appendChild(div_title);
+	a.appendChild(div);
+	dynamic.appendChild(a);
+}
