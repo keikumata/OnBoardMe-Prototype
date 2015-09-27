f43fa40e3d51610b458c8fac78fa6c0d6a75d65a
keikumata <ky52@duke.edu>
2015-09-27 04:36:02 +0100

    finished events

--- client/js/events.js
+++ client/js/events.js
@@ -27,39 +27,71 @@
   	city_title.className = "title";
   	city_title.innerHTML = city.name;
 
-  	// var city_image = document.getElementById("city-image");
-  	// city_image.className="city-image";
-  	// city_image.style.backgroundImage = "url('"+city.img+"')";
+  	var city_image = document.getElementById("city-image");
+  	city_image.className="city-image";
+  	city_image.style.backgroundImage = "url('"+city.img+"')";
   	
 
   	var city_title_stick = document.getElementById("title-stick");
   	city_title_stick.className = "title-stick";
   	city_title_stick.innerHTML = city.name;
 
-  	// var city_image_stick= document.getElementById("city-image-stick");
-  	// city_image.className="city-image-stick";
-  	// city_image.style.backgroundImage = "url('"+city.img+"')";
+  	var city_image_stick= document.getElementById("city-image-stick");
+  	city_image_stick.className="city-image-stick";
+  	city_image_stick.style.backgroundImage = "url('"+city.img+"')";
+
+  	var dynamic_event = document.getElementById("dynamic-event");
 
   	for (var i = 0 ; i<names.length;i++) {
+  		var a = document.createElement("a");
+  		a.href = "eventinfo.html?"+"aid=" + aids[i];
+
   		var attraction = document.createElement("div");
-  	attraction.className="event";
+  		attraction.className="event";
 
-  	var event_image = document.createElement("div");
-  	event_image.className="event-image";
-  	event_image.style.backgroundImage = "url('"+urls[i]+"')";
+  		var event_image = document.createElement("div");
+  		event_image.className="event-image";
+  		event_image.style.backgroundImage = "url('"+urls[i]+"')";
 
+  		var event_title_wrapper = document.createElement("div");
+  		event_title_wrapper.className="event-title-wrapper";
 
+  		var event_title = document.createElement("div");
+  		event_title.className="event-title";
+  		event_title.innerHTML = names[i];
+
+  		var event_location = document.createElement("div");
+  		event_location.className="event-location";
+
+  		var map_icon = document.createElement("i");
+  		map_icon.className="fa fap-map-marker";
+
+  		event_location.appendChild(map_icon);
+  		var event_location_name = document.createTextNode(" " + loc[i]);
+  		event_location.appendChild(event_location_name);
+
+  		var event_price = document.createElement("div");
+  		event_price.className="event-price";
+
+  		var money_icon = document.createElement("i");
+  		money_icon.className = "fa fa-money";
+
+  		event_price.appendChild(money_icon);
+  		var event_price_value = document.createTextNode(" " + price[i]);
+  		event_price.appendChild(event_price_value);
+
+  		var clear = document.createElement("div");
+  		clear.className = "clear";
+
+  		event_title_wrapper.appendChild(event_title);
+  		event_title_wrapper.appendChild(event_location);
+  		event_title_wrapper.appendChild(event_price);
+
+  		event_image.appendChild(event_title_wrapper);
+  		event_image.appendChild(clear);
+  		attraction.appendChild(event_image);
+  		a.appendChild(attraction);
+  		dynamic_event.appendChild(a);
   	}
   	
   }
-  
-// <div class="event">
-// <div class="event-image" style="background-image:url('http://camaje.com/-/wp-content/uploads/2013/06/camaje-cooking-7.jpg')">
-//             <div class="event-title-wrapper">
-//               <div class="event-title">Recipease Jamie Oliver</div>
-//               <div class="event-location"><i class="fa fa-map-marker"></i> Camden</div>
-//               <div class="event-price"><i class="fa fa-money"></i> £ 50</div>
-//             </div>
-//             </div>
-//             <div class="clear"></div>
-//           </div>
